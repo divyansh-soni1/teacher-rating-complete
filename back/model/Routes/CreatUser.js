0dc2299cce1c6dc28b177ccc52793b94d53a4a49
Divyansh soni <97580995+divyansh-soni1@users.noreply.github.com>
2023-03-26 19:29:03 +0530

    Add files via upload

---
+++ back/model/Routes/CreatUser.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../model/User')
+const { body, validationResult } = require('express-validator');
+
+
+router.post("/creatuser",
+
+
+ [body('email').isEmail(),
+body('password').isLength({ min: 5 })
+ ],
+
+
+async(req,res)=>{
+     
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try{
+        await User.create({
+            name: req.body.name,
+            password:req.body.password,
+            email: req.body.email,
+            location : req.body.location
+
+        }).then(
+        res.json({success:true}));
+    }
+    catch(error){
+        console.log(error)
+        res.json({success:false});
+
+    }
+})
+
+module.exports = router;
